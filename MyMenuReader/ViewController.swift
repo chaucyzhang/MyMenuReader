de4359d2d43781a149b68c48fd270587d0fb53f6
xizhang <chaucyzhang@gmail.com>
2018-01-10 20:26:19 -0800

    Initial Commit

---
+++ MyMenuReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMenuReader
+//
+//  Created by xi zhang on 1/10/18.
+//  Copyright © 2018 xi zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
